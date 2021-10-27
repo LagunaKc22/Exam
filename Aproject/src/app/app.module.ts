f35697ccc257f42833e8fea56a9f51f449d81314
LagunaKc22 <kcphilip.laguna.evsu.edu.ph>
2021-10-27 22:27:04 +0700

    added all

---
+++ Aproject/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { MainComponent } from './pages/main/main.component';
+const routes: Routes =[
+  {path: "main", component:MainComponent,
+    children:[
+      {path: "home", component: HomeComponent},
+      {path: "profile", component: ProfileComponent},
+      {path: "gallery", component: GalleryComponent},
+      {path: "contact", component: ContactComponent}
+    ]
+  },
+  {path:"**", redirectTo:"main"}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ContactComponent,
+    GalleryComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
